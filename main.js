fb80bd96f878b85662a4176811d153fde492fe62
juancmariani <131925554+juancmariani@users.noreply.github.com>
2023-06-19 18:14:39 -0300

    PreEntrega3Mariani

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
-//Simulador inflación vs plazo fijo => No soy experto en economía, por lo que algún cálculo puede ser diferente a la realidad.
 function calcularPlazo (montAct, values) {
-    let valorInflacionado = values.inversion + values.inversion * values.inflacionInteranual;
+    let valorInflacionado = values.inversion + values.inversion * values.inflacion;
     let resultadoAnual = values.inversion + values.tasaAnual * values.inversion;
     let difValor = montAct - valorInflacionado;
     let difValor2 = resultadoAnual - valorInflacionado;
     if (montAct > valorInflacionado) {
-        alert("A esta inflación y con esta tasa, te conviene realizar un plazo fijo y reinvertir los dividendos en el mismo.\nPor un beneficio de: $" 
+        document.getElementById("result").innerHTML = ("A esta inflación y con esta tasa, te conviene realizar un plazo fijo y reinvertir los dividendos en el mismo. \n Por un beneficio de: $" 
             + (montAct - values.inversion).toFixed(2) + "\nLe estás ganando $" + difValor.toFixed(2) + " a la Inflación.\n\nEso sí, bajo tu propio riesgo que esto es Argentina...");
-    } else {
-        alert("Ni te gastes en hacer el plazo fijo, compra dólares. Estarías perdiendo un valor de $" + Math.abs((difValor)));
-    } 
-    if (resultadoAnual > valorInflacionado) {
-        alert("También te conviene realizar el plazo fijo aunque retires los dividendos. Le estarías ganando $" + difValor2 + " a la inflación.");
-    } else {
-        alert("Si retiras los dividendos mes a mes, no te conviene hacer el plazo fijo. Compra dólares para no perder un valor de $" + Math.abs((difValor2)));
+    }   else {
+        document.getElementById("result").innerHTML = ("Ni te gastes en hacer el plazo fijo, compra dólares. Estarías perdiendo un valor de $" + Math.abs((difValor)));
     }
+
+    if (resultadoAnual > valorInflacionado) {
+        document.getElementById("result2").innerHTML = ("También te conviene realizar el plazo fijo aunque retires los dividendos. Le estarías ganando $" + difValor2 + " a la inflación.");
+    } else {
+        document.getElementById("result2").innerHTML = ("Si retiras los dividendos mes a mes, no te conviene hacer el plazo fijo. Compra dólares para no perder un valor de $" + Math.abs((difValor2)));
+    }
+    localStorage.clear()
 }
-    
+
 function outputMessage(arr) {
     let informacionMes = "";
     arr.forEach(valueMonth => informacionMes += "Mes " + valueMonth.mes + ": Monto acumulado de $" + valueMonth.montoActual + "\n");
     return informacionMes;
 }
 
-let inputValues = {
-    tasaAnual: parseFloat(prompt("Ingrese la tasa anual para la constitución de un plazo fijo: \n\nTip: Algunos bancos la tienen en 91%, otros en 97%")),
-    inflacionInteranual: parseFloat(prompt("Ingrese la inflación interanual: \n\nTip: La del mes de abril fue del 108,8%")),
-    inversion: parseFloat(prompt("Cuánto dinero estás dispuesto a invertir?"))
-};
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    localStorage.setItem("tasaAnual", document.getElementById("tasaAnual").value);
+    localStorage.setItem("inflacion", document.getElementById("inflacion").value);
+    localStorage.setItem("inversion", document.getElementById("inversion").value);
+    location.reload();
+})
 
+let inputValues = {
+tasaAnual: parseFloat(localStorage.getItem("tasaAnual")),
+inflacion: parseFloat(localStorage.getItem("inflacion")),
+inversion: parseFloat(localStorage.getItem("inversion"))
+}
 
 if (inputValues.tasaAnual > 3) {
     inputValues.tasaAnual = inputValues.tasaAnual / 100;
 }
 
-if (inputValues.inflacionInteranual > 20) {
-    inputValues.inflacionInteranual = inputValues.inflacionInteranual / 100;
+if (inputValues.inflacion > 20) {
+    inputValues.inflacion = inputValues.inflacion / 100;
 }
 
 let tasaMensual = inputValues.tasaAnual / 12;
 let beneficioMensual = 0;
 let montoActual = inputValues.inversion;
 let arrInfoMes = [];
-
 
 for (let mes = 1; mes <=12; mes++) {
     montoActual += montoActual * tasaMensual;
@@ -51,5 +58,10 @@ for (let mes = 1; mes <=12; mes++) {
 
 let infoMes = outputMessage(arrInfoMes);
 
-alert("Si reinvertís los dividendos del plazo fijo mes a mes, estos serían los valores que recuperarías cada mes: \n\n" + infoMes);
-calcularPlazo(montoActual, inputValues);
+console.log("Si reinvertís los dividendos del plazo fijo mes a mes, estos serían los valores que recuperarías cada mes: \n\n" + infoMes);
+calcularPlazo(montoActual, inputValues); 
+
+function clear() {
+    localStorage.clear()
+}
+clear()
